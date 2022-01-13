562b960e305c54d87495fbf6306988cb9312162e
Bhawna Gupta <programmingwithgupta@gmail.com>
2022-01-14 00:19:49 +0530

    Array Assignment : Reverse a array

---
+++ Arrays/Basic/ReverseArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Solution
+{
+    public List<int> solve(List<int> A)
+    {
+        int temp, i, j;
+        int n = A.Count;
+        i = 0;
+        j = n - 1;
+        while (i < j)
+        {
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            i++;
+            j--;
+        }
+        return A;
+    }
+}
+
+//TC : O(N/2)
